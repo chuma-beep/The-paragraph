0c36b634e9ba0af7585ee9d451e0249e41b8ab6b
chuma-beep <wisboynelson123@gmail.com>
2024-10-08 19:41:07 +0100

    fixed login responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ import Search from '@/components/Search/Search'
 export default function Index() {
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+        <div className="w-full max-w-4xl flex items-center text-sm gap-0">
           <DeployButton />
-          <div className="flex flex-row-reverse w-full items-end space-y-4 justify-items-center ">
+          <div className="flex flex-row-reverse w-full items-end   ">
                   <WriteIcon />
               <Search/>
           </div>
--- components/WriteIcon.tsx
+++ components/WriteIcon.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export function WriteIcon() {
   return (
-    <Button variant="outline" className="bg-white mx-2 text-primary rounded-full flex flex-row hover:text-blue-300">
+    <Button variant="outline" className="bg-white text-primary rounded-full flex flex-row hover:text-blue-300">
       <Link
       href="/protected/account/write"     
       >
