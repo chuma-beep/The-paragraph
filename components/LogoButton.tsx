f899c2c4faf22e9cc579d0a03361d746d9bcf1a2
chuma-beep <wisboynelson123@gmail.com>
2024-10-12 01:41:52 +0100

    fixed logo in small screens

--- components/LogoButton.tsx
+++ components/LogoButton.tsx
@@ -3,17 +3,8 @@ import ParagraphLogo from './ParagrapghLogo';
 export default function LogoButton() {
   return (
     <>
-    {/* <div className='w-full flex flex-row justify-center'>
-
-    <span
-      className="w-12 h-12 mt-2 flex items-center justify-center rounded-full no-underline hover:bg-btn-background-hover border"
-      >
-      <ParagraphLogo />
-    </span>
-      <h3>The Paragraph</h3>
-        </div> */}
         
-        <div className="w-full max-w-md mx-2 ">
+        <div className="w-full max-w-md mr-2 ml-0">
       <div className="flex items-center">
         <div className="flex-shrink-0">
           <span className="inline-flex items-center justify-center w-12 h-12 rounded-full transition-colors duration-200 cursor-pointer hover:border-blue-400">
