18caf750ef1dec1edf649727d55498a092d0fdea
chuma-beep <wisboynelson123@gmail.com>
2024-10-13 12:40:42 +0100

    added sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import MainFooter from "@/components/main-footer";
 import PostList from "@/app/postlist/PostList";
 import { WriteIcon } from '@/components/WriteIcon';
 import Search from '@/components/Search/Search'
+import { ComponentsBlogSidebar } from "@/components/BlogSideBar";
 
 
 
@@ -25,6 +26,11 @@ export default function Index() {
         </div>
       </nav>
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+         {/* Sidebar */}
+         <div className="flex mr-12">
+            <ComponentsBlogSidebar />
+          </div>
+          
         <main className="flex-1 flex flex-col gap-6 px-4">
           <PostList />
         </main>
