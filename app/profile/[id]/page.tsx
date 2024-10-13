90d144d2f3103db35b7a0ddc5e1db104e19b46f6
chuma-beep <wisboynelson123@gmail.com>
2024-10-13 15:55:21 +0100

    bg fix

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -148,7 +148,7 @@ const UserProfile = () => {
   return (
     <div  className="p-16">
           <BackButton/>
-      <div className="p-8 bg-white shadow mt-24">
+      <div className="p-8 bg-tranparent shadow mt-24">
         <div className="grid grid-cols-1 md:grid-cols-3">
           <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
             {/* <div>
@@ -171,7 +171,7 @@ const UserProfile = () => {
                   className="rounded-full h-48 w-48"
                 />
               ) : (
-                <div className="w-48 h-48 bg-gray-200 rounded-full"></div>
+                <div className="w-48 h-48 bg-transparent rounded-full"></div>
               )}
             </div>
           </div>
