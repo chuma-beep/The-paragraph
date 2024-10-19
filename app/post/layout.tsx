0a633809a4574a64e4da9aab45c9cc425407814d
chuma-beep <wisboynelson123@gmail.com>
2024-10-20 00:42:05 +0100

    adjusted navbar in single post view

--- app/post/layout.tsx
+++ app/post/layout.tsx
@@ -13,11 +13,14 @@ import { ModeToggle } from "@/components/toggle-theme";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-scree min-w-full flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-white sm:p-0 px-12">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 sm:p-0 px-12 pl-0">
+    <div className="min-h-screen min-w-full sm:w-full flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 sm:p-0 px-16 pl-0">
       <div className="w-full max-w-4xl flex items-center text-sm gap-0">
+        <span className="sm:ml-0 ml-12 w-full">
+
           <DeployButton />
-          <div className="flex flex-row-reverse w-full items-end mr-2 gap-2">
+        </span>
+          <div className="flex flex-row-reverse w-full items-end mr-2 gap-1">
             <ModeToggle/>
           <WriteIcon />
           <Search/>
@@ -27,13 +30,14 @@ export default function Layout({ children }: { children: ReactNode }) {
       </nav>
 
       {/* Main content */}
-      <main className="flex-grow container mx-auto px-4 py-8">
+      {/* <main className="flex-grow container mx-auto px-4 py-8"> */}
+      <main className="w-full">
         {children}
       </main>
       
       {/* Footer */}
       <footer className="bg-gray-100 dark:bg-gray-800 py-4 mt-8">
-        <div className="max-w-6xl mx-auto px-4 text-center text-sm">
+        <div className="flex px-4 text-center text-sm">
         <MainFooter />
         </div>
       </footer>
