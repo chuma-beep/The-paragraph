5da352741cd0fffac8d3830eb72be164b6bf9ec4
chuma-beep <wisboynelson123@gmail.com>
2024-10-28 07:39:37 +0100

    changed personal page

--- app/protected/account/LoggedInUser.tsx
+++ app/protected/account/LoggedInUser.tsx
@@ -13,39 +13,6 @@ export default function LoggedInUser() {
   const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
   const [user, setUser] = useState<User | null>(null);
 
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     const { data: userData, error: userError } = await supabase.auth.getUser();
-
-  //     if (userError) {
-  //       console.error('Error fetching user:', userError.message);
-  //       return;
-  //     }
-
-  //     if (userData?.user) {
-  //       setUser(userData.user);
-
-  //       const { data: profileData, error: profileError } = await supabase
-  //         .from('profiles')
-  //         .select('avatar_url')
-  //         .eq('id', userData.user.id)
-  //         .single();
-
-  //       if (profileError) {
-  //         console.error('Error fetching user profile:', profileError.message);
-  //       } else if (profileData?.avatar_url) {
-  //         // Generate the full URL for the avatar image
-  //         const { data } = supabase.storage.from('avatars').getPublicUrl(profileData.avatar_url);
-  //         if (data?.publicUrl) {
-  //           setAvatarUrl(data.publicUrl); // Set the public URL for the image
-  //         }
-  //       }
-  //     }
-  //   };
-
-  //   fetchUser();
-  // }, []);
-
 
   useEffect(() => {
     const fetchUser = async () => {
