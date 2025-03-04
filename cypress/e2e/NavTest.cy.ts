3aa8aaea5a9513d7bfa64a82768398b81f6aefea
chuma-beep <wisboynelson123@gmail.com>
2025-03-04 07:27:08 +0100

    fixing testing

--- cypress/e2e/NavTest.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Navigation', () => {
-    it('should navigate to login page', () => {
-      cy.visit('http://localhost:3000/login');
-      cy.get('[data-testid="signup-link"]').click();
-      cy.url().should('include', '/signup');
-      cy.contains('Sign up'); 
-    });
-  });
-  
