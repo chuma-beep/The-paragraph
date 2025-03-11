b46907c8f52f2bcadf5030ee8533c05449e1042d
chuma-beep <wisboynelson123@gmail.com>
2025-03-11 18:24:57 +0100

    added protection to middleware

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,36 @@ import { updateSession } from "@/utils/supabase/middleware";
 import { NextResponse } from "next/server";
 import { createClient } from "./utils/supabase/client";
 import { supabase } from "./services/supabaseClient";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-  if (session && request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/protected", request.url));
+  const response = NextResponse.next()
+  const supabase = createMiddlewareClient({ req: request, res: response})
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+    //protected route check
+
+  const protectedRoutes = ['/dashboard', '/profile', '/settings']
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  )
+
+  if (isProtectedRoute && !session ){
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-  return NextResponse.next();
+  return response
 }
+
+//   if (session && request.nextUrl.pathname === "/") {
+//     return NextResponse.redirect(new URL("/protected", request.url));
+//   }
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: [
